406f4b81c36a6b93f00cd3d2e9663dc6f81e1548
ArtiomVert <asayarov@gmail.com>
2025-07-16 15:50:51 +0300

    Initial commit

---
+++ src/test/java/ru/kpfu/itis/cmsforblogs/CmSforBlogsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kpfu.itis.cmsforblogs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmSforBlogsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
